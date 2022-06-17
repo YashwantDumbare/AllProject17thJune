e623906d5983f80964074bed0054c8a42cf88b68
YashwantDumbare <yashwantdumbare7@gmail.com>
2022-06-17 13:57:55 +0530

    17th june

---
+++ MVNNewProj/src/main/java/testing/MVNNewProj/App.java
@@ -0,0 +1,13 @@
+package testing.MVNNewProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MVNNewProj/src/test/java/testing/MVNNewProj/Sample.java
@@ -0,0 +1,48 @@
+package testing.MVNNewProj;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+	@Test
+	public void sampleOne() throws IOException {
+		FileInputStream file = new FileInputStream(".\\src\\test\\Book1.xlsx");
+		Workbook wrkbk = new XSSFWorkbook(file);
+		int sheetcount = wrkbk.getNumberOfSheets();
+		System.out.println(sheetcount);
+		String sheetname = wrkbk.getSheetName(0);
+		System.out.println(sheetname);
+		Sheet sheet1 = wrkbk.getSheetAt(0);
+		System.out.println(sheet1.getLastRowNum());
+		Row row = sheet1.getRow(1);
+		Cell cell = row.getCell(0);
+		Cell cell2 = row.getCell(1);
+		Cell cell3 = row.getCell(2);
+		String cellvalue = cell.getStringCellValue();
+		String cellvalue2=cell2.getStringCellValue();
+		String cellvalue3=cell3.getStringCellValue();
+		System.out.println(cellvalue);
+		System.out.println(cellvalue2);
+		System.out.println(cellvalue3);
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get(cellvalue);
+		driver.switchTo().activeElement().sendKeys(cellvalue2,Keys.TAB);
+		driver.switchTo().activeElement().sendKeys(cellvalue3,Keys.TAB);
+		driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+		driver.close();
+
+}
+}
